07640670b90b03b1253ce045efd13592b2171a3c
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-06-11 15:17:01 -0500

    sort.h

--- sort.h
+++ sort.h
@@ -1,6 +1,12 @@
 #ifndef _SORT_H_
 #define _SORT_H_
 
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -22,9 +28,7 @@ void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
 
-
-
-
-
+/**/
+void bubble_sort(int *array, size_t size);
 
 #endif _SORT_H_
